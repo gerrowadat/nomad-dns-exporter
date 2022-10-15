dd137d5486b926fe98468f507a3df8cda1334b98
Dave O'Connor <git@mail.andvari.net>
2022-10-15 02:12:55 +0100

    Initial hacky UDP version that Works Just Fine(tm)

--- nomad-dns-exporter.py
+++ nomad-dns-exporter.py
@@ -1,59 +1,85 @@
 import nomad
-import socket
+import asyncio
+import asyncio_dgram
 import dnslib
+import logging
 import argparse
 
 
-def main():
+ARGS = None
+
+
+async def udp_server(ip, port):
+    logging.debug('Binding to %s UDP port %d' % (ip, port))
+    udp_stream = await asyncio_dgram.bind((ip, port))
+
+    while True:
+        (data, remote) = await udp_stream.recv()
+        rep = resolve_nomad(ARGS.nomad_server, data)
+        await udp_stream.send(rep.pack(), remote)
+
+
+async def main():
     p = argparse.ArgumentParser()
     p.add_argument('--nomad_server', dest='nomad_server',
                    type=str, default='hedwig')
     p.add_argument('--dns_port', dest='dns_port', type=int,
                    default=5333)
+    p.add_argument('--dns_ttl', dest='dns_ttl', type=int,
+                   default=3600)
     p.add_argument('--nomad_domain', dest='nomad_domain', type=str,
-                   default='.service.nomad.')
+                   default='.service.nomad')
     args = p.parse_args()
+    global ARGS
+    ARGS = args
 
-    udp_server = socket.socket(family=socket.AF_INET,
-                               type=socket.SOCK_DGRAM)
-    udp_server.bind(("127.0.0.1", args.dns_port))
-
-    while True:
-        (msg, addr) = udp_server.recvfrom(1024)
-        req = dnslib.DNSRecord.parse(msg.strip())
-        rep = dnslib.DNSRecord(dnslib.DNSHeader(
-                               id=req.header.id,
-                               qr=1,
-                               aa=1,
-                               ra=1),
-                               q=req.q)
-        print('QN: %s' % (str(req.q.qname)))
-        print(req)
-        ips = resolve(args.nomad_server, str(req.q.qname)[:-1])
-        for ip in ips:
-            rep.add_answer(
-                           dnslib.RR(str(req.q.qname), rdata=dnslib.A(ip)))
-        udp_server.sendto(rep.pack(), addr)
+    udps = udp_server('0.0.0.0', args.dns_port)
+    await asyncio.gather(udps)
 
 
-def resolve(nomad_server, query):
+def resolve_nomad(nomad_server, dns_req):
+    req = dnslib.DNSRecord.parse(dns_req)
+    rep = dnslib.DNSRecord(dnslib.DNSHeader(
+                           id=req.header.id,
+                           qr=1,
+                           aa=1,
+                           ra=1),
+                           q=req.q)
+
+    query = str(req.q.qname)[:-1]
+
+    if not query.endswith(ARGS.nomad_domain):
+        logging.warning(
+            'NXDOMAIN (outside %s domain) for %s' % (
+                ARGS.nomad_domain, query))
+        return rep
+
     n = nomad.Nomad(host=nomad_server)
     all_job_names = [j['Name'] for j in n.jobs.get_jobs()]
     all_allocs = n.allocations.get_allocations()
 
-    if query not in all_job_names:
-        print('NXDOMAIN %s' % (query))
-        return 1
+    # Strip the nomad domain off the query.
+    svc_query = query[:(len(query) - len(ARGS.nomad_domain))]
+
+    if svc_query not in all_job_names:
+        logging.warning('NXDOMAIN for %s' % (svc_query))
+        return rep
 
     job_allocs = [a for a in all_allocs
                   if a['ClientStatus'] == 'running' and
-                  a['JobID'] == query]
+                  a['JobID'] == svc_query]
     ips = []
     for alloc in job_allocs:
         node = n.node.get_node(alloc['NodeID'])
         ips.append(node['Attributes']['unique.network.ip-address'])
-    return ips
+    logging.info('%s: %s' % (query, ips))
+    for ip in ips:
+        rep.add_answer(
+                       dnslib.RR(
+                           str(req.q.qname),
+                           rdata=dnslib.A(ip),
+                           ttl=ARGS.dns_ttl))
+    return rep
 
 
-if __name__ == '__main__':
-    main()
+asyncio.run(main())
