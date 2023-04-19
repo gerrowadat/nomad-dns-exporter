fc0f0de7660620d50485e20860124a8c09b754e2
Dave O'Connor <git@mail.andvari.net>
2023-04-20 00:21:19 +0100

    add --dns_hostname option

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-"$@" /nomad-dns-exporter/nomad-dns-exporter.py --nomad_server=${NOMAD_SERVER} --dns_port=${DNS_PORT} --dns_ttl_secs=${DNS_TTL} --nomad_domain=${NOMAD_DOMAIN} --alsologtostderr
+"$@" /nomad-dns-exporter/nomad-dns-exporter.py --nomad_server=${NOMAD_SERVER} --dns_hostname=${DNS_HOSTNAME} --dns_port=${DNS_PORT} --dns_ttl_secs=${DNS_TTL} --nomad_domain=${NOMAD_DOMAIN} --alsologtostderr
